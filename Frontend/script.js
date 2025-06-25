77ccf059c8b27d35693cf0cb23106378f2376f75
xolani-tech <jonathanmicah23@gmail.com>
2025-06-25 14:37:10 +0200

    second commit

--- Frontend/script.js
+++ Frontend/script.js
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const slides = [
         {
-            image: 'New Jesalem Internatinal Ministires\Images\NJ picture man of god.jpg',
+            image: 'https://scontent-cpt1-1.xx.fbcdn.net/v/t39.30808-6/489379034_1096403432505998_1006130090780695940_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFiIyj_UsaB5Uc7VOW9lSNYFtCIj6fdAjkW0IiPp90COeBM7ScV0l2fmRXr2L8RjQW8DEAzfCcshrj9kHFhmcVL&_nc_ohc=csvoCfghmSUQ7kNvwFvSGTz&_nc_oc=AdnRJmmw8hpPzLFt9FHRB9fi5rJSsqLTAUIbfT4RJNn0zvoPutLLUeOFaOy1zdqQqJY&_nc_zt=23&_nc_ht=scontent-cpt1-1.xx&_nc_gid=bDjxGWrbg1_FAseQzWkr1A&oh=00_AfN9kmkXkAbHDDqGHuevWIIBvgqbwOmJhQzkn6WyaZ08kA&oe=68617878',
             scripture: 'Welcome to',
             title: 'New Jerusalem Church Of All Nations',
             subtitle: 'The spirit of GOD lives'
@@ -65,13 +65,13 @@ document.addEventListener('DOMContentLoaded', function() {
             subtitle: 'sharper than any two-edged sword, piercing to the division of soul and of spirit...'
         },
         {
-            image: 'https://images.unsplash.com/photo-1457369804613-52c61a468e7d?q=80&w=2070&auto=format&fit=crop',
+            image: 'https://scontent-cpt1-1.xx.fbcdn.net/v/t39.30808-6/489231075_1095625295917145_681549800403990065_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFQgOk2dc37mHjWHa57f4CQemH68aAxdet6YfrxoDF16_C0J9vTuHintaWm8qBOPdumLiR7Q-n6n4XpYcANTE4F&_nc_ohc=MYb8raHjMmsQ7kNvwElze9d&_nc_oc=Adl20f4833stuAbpXfLVJmOFaXcq--VHvWbic3pbHYIPLY__WTQNkol8Cfdl1IsWCgo&_nc_zt=23&_nc_ht=scontent-cpt1-1.xx&_nc_gid=BNGQ4NjbwEZAHbGjByAfyg&oh=00_AfNBFlMyr7CHhpcz_NMesV-xasKP2NwTAYABHQ81cdMjBA&oe=68616F9A',
             scripture: 'John 3:16',
             title: 'For God So Loved The World,',
             subtitle: 'that he gave his only Son, that whoever believes in him should not perish but have eternal life.'
         },
         {
-            image: 'https://images.unsplash.com/photo-1516846391414-274287398c99?q=80&w=2070&auto=format&fit=crop',
+            image: 'https://scontent-cpt1-1.xx.fbcdn.net/v/t39.30808-6/488608909_1096100572536284_4788623712891631385_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeHjAf1xb8i-As9N2KrugM4TCRWb6ryvDywJFZvqvK8PLNR1kHjMkORqoLgpQ6wTvT8JClJxPmv9d1D9g71-iC8r&_nc_ohc=SS38VpOjhg4Q7kNvwGkHGxI&_nc_oc=Adm6nyha6e_oFU9VIpGa-b03hbkLTvt-UnURFmxfhYa_91GyCbKa5-XTbisqiHDXd2Q&_nc_zt=23&_nc_ht=scontent-cpt1-1.xx&_nc_gid=_M5wToT5D4OZlaXUWgMG7g&oh=00_AfM_uywKmsFml-_qBtPRw3kIfxH5TSJ8jzA3Drw8p1Xb0w&oe=68616611',
             scripture: 'Romans 8:28',
             title: 'And We Know That For Those Who Love God',
             subtitle: 'all things work together for good, for those who are called according to his purpose.'
