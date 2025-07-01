fd69944c2b79caf8db99b5cb7ad8c3a16d726e9c
xolani-tech <jonathanmicah23@gmail.com>
2025-07-01 09:47:52 +0200

    second push

--- Frontend/script.js
+++ Frontend/script.js
@@ -52,26 +52,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const slides = [
         {
-            image: 'https://scontent-cpt1-1.xx.fbcdn.net/v/t39.30808-6/489379034_1096403432505998_1006130090780695940_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFiIyj_UsaB5Uc7VOW9lSNYFtCIj6fdAjkW0IiPp90COeBM7ScV0l2fmRXr2L8RjQW8DEAzfCcshrj9kHFhmcVL&_nc_ohc=csvoCfghmSUQ7kNvwFvSGTz&_nc_oc=AdnRJmmw8hpPzLFt9FHRB9fi5rJSsqLTAUIbfT4RJNn0zvoPutLLUeOFaOy1zdqQqJY&_nc_zt=23&_nc_ht=scontent-cpt1-1.xx&_nc_gid=bDjxGWrbg1_FAseQzWkr1A&oh=00_AfN9kmkXkAbHDDqGHuevWIIBvgqbwOmJhQzkn6WyaZ08kA&oe=68617878',
+            image: 'https://scontent-cpt1-1.xx.fbcdn.net/v/t39.30808-6/487048289_1089767566502918_7316173163832005464_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEWofudGZyiAQULs9QrFIpoLEciTwpNJ1gsRyJPCk0nWCHwihAonotJjj7ITFgfEUaEcjPSv5fKjAtCfHoAIyRk&_nc_ohc=yjyWevcs-GwQ7kNvwH0KEo0&_nc_oc=AdkWWu9eD2EV75DVvWrJCL4ro_o8JCwWhs8lQqqb8B9yU88G6Ph8qRZqBS9ACdq-DlI&_nc_zt=23&_nc_ht=scontent-cpt1-1.xx&_nc_gid=703-U_fE5kezTmbPJFqU1A&oh=00_AfMhDGSqPqpKKMg00kvmcjQEm5rSe634v_pdcVrVZYe7RQ&oe=68685C55',
             scripture: 'Welcome to',
             title: 'New Jerusalem Church Of All Nations',
             subtitle: 'The spirit of GOD lives'
 
         },
         {
-            image: 'https://th.bing.com/th/id/R.64cbcd1d020a0d53fcf7e57e844c45ee?rik=GF64nbnGn%2baU6g&riu=http%3a%2f%2fi.huffpost.com%2fgen%2f772790%2fimages%2fo-BIBLE-facebook.jpg&ehk=6%2bljvZHQ%2fFMrOuITZGA%2f0aihHT3oeFA4MERw2rwdp4s%3d&risl=&pid=ImgRaw&r=0',
+            image: 'https://scontent-cpt1-1.xx.fbcdn.net/v/t39.30808-6/489025202_1094585086021166_2597828619559256447_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGhv3v5S1Dryrwp380FzCBFHShGbVnqLIQdKEZtWeoshPcBpfNrSoNIaLvjlmv2XN5m3UIpSwu8p0FAEIVKprkV&_nc_ohc=IWA4hp7MPasQ7kNvwHhwIto&_nc_oc=Admz7o2nQlieM2NKIOQBzOR8rZX3-EiWaW0gqx9-BFzxG1V8eMYGMC3P5jcidiFcbfE&_nc_zt=23&_nc_ht=scontent-cpt1-1.xx&_nc_gid=vyesgSQw4_QHMiD781Fmrw&oh=00_AfMxWXXFz_HH_JKLlWa5qux8Y4RnWTerzIK-Ww7e_JMQ_w&oe=686855FA',
             scripture: 'Hebrews 4:12',
             title: 'For The Word Of God Is Living And Active,',
             subtitle: 'sharper than any two-edged sword, piercing to the division of soul and of spirit...'
         },
         {
-            image: 'https://scontent-cpt1-1.xx.fbcdn.net/v/t39.30808-6/489231075_1095625295917145_681549800403990065_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFQgOk2dc37mHjWHa57f4CQemH68aAxdet6YfrxoDF16_C0J9vTuHintaWm8qBOPdumLiR7Q-n6n4XpYcANTE4F&_nc_ohc=MYb8raHjMmsQ7kNvwElze9d&_nc_oc=Adl20f4833stuAbpXfLVJmOFaXcq--VHvWbic3pbHYIPLY__WTQNkol8Cfdl1IsWCgo&_nc_zt=23&_nc_ht=scontent-cpt1-1.xx&_nc_gid=BNGQ4NjbwEZAHbGjByAfyg&oh=00_AfNBFlMyr7CHhpcz_NMesV-xasKP2NwTAYABHQ81cdMjBA&oe=68616F9A',
+            image: 'https://scontent-cpt1-1.xx.fbcdn.net/v/t39.30808-6/489231075_1095625295917145_681549800403990065_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFQgOk2dc37mHjWHa57f4CQemH68aAxdet6YfrxoDF16_C0J9vTuHintaWm8qBOPdumLiR7Q-n6n4XpYcANTE4F&_nc_ohc=SineQAdM0ckQ7kNvwFPdzkO&_nc_oc=Adl5Ptxwy3pkiFZ5u2s_aEaB5D4hUsvJPFmeVlq248btUMlRnZxv6OKwtePIrI1V30g&_nc_zt=23&_nc_ht=scontent-cpt1-1.xx&_nc_gid=DMFIH2ebSsB88Nt_phJHLg&oh=00_AfNZL3JlXzMLBh0eFPHAquQ14FnPhUjziVJtBKPxaMIrIQ&oe=6868779A',
             scripture: 'John 3:16',
             title: 'For God So Loved The World,',
             subtitle: 'that he gave his only Son, that whoever believes in him should not perish but have eternal life.'
         },
         {
-            image: 'https://scontent-cpt1-1.xx.fbcdn.net/v/t39.30808-6/488608909_1096100572536284_4788623712891631385_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeHjAf1xb8i-As9N2KrugM4TCRWb6ryvDywJFZvqvK8PLNR1kHjMkORqoLgpQ6wTvT8JClJxPmv9d1D9g71-iC8r&_nc_ohc=SS38VpOjhg4Q7kNvwGkHGxI&_nc_oc=Adm6nyha6e_oFU9VIpGa-b03hbkLTvt-UnURFmxfhYa_91GyCbKa5-XTbisqiHDXd2Q&_nc_zt=23&_nc_ht=scontent-cpt1-1.xx&_nc_gid=_M5wToT5D4OZlaXUWgMG7g&oh=00_AfM_uywKmsFml-_qBtPRw3kIfxH5TSJ8jzA3Drw8p1Xb0w&oe=68616611',
+            image: 'https://scontent-cpt1-1.xx.fbcdn.net/v/t39.30808-6/487407473_1092503219562686_8647403533553577986_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeHqg6L6mgVncp2yWEAfJ_gIBu9MWN-xrkoG70xY37GuStMSuW_1TQaVPmf-EIa4cUboL2Z8fGUN9x3JmhLnfmwh&_nc_ohc=YWLoQcApV-QQ7kNvwGf3NX_&_nc_oc=AdmVce624D6m5EpZsxuzRu0zZLTvaJe1axTLU_9sxTkno1ke2KgBzxQ24m3EW6QQFF4&_nc_zt=23&_nc_ht=scontent-cpt1-1.xx&_nc_gid=ms5mNBXmAVabJhXHmjuF2g&oh=00_AfOCTLZQrYAQpFw48Hku-tJ-mNhzM4FRdGbNDvyYc3VAgQ&oe=68685659',
             scripture: 'Romans 8:28',
             title: 'And We Know That For Those Who Love God',
             subtitle: 'all things work together for good, for those who are called according to his purpose.'
